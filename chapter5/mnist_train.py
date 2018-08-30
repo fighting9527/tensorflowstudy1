2903a270d3cddd88fb37c4e284411b3093747ac7
caoqi <caoqi5234617@sina.com>
2018-08-30 22:04:03 +0800

    mnist数字识别优化
    mnist数字识别优化

---
+++ chapter5/mnist_train.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+import os
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+# 加载mnist_inference.py中定义的常量和前向传播函数
+import chapter5.mnist_inference as mnist_inference
+
+# 配置神经网络的参数
+BATCH_SIZE = 100
+LEARNING_RATE_BASE = 0.8
+LEARNING_RATE_DECAY = 0.99
+REGULARIZATION_RATE = 0.0001
+TRAINING_STEPS = 30000
+MOVING_AVERAGE_DECAY = 0.99
+# 模型保存的路径和文件名
+MODEL_SAVE_PATH = "mnist_model/"
+MODEL_NAME = "model.ckpt"
+
+def train(mnist):
+    # 定义输入输出placeholder
+    x = tf.placeholder(tf.float32, [None, mnist_inference.INPUT_NODE], name='x-input')
+    y_ = tf.placeholder(tf.float32, [None, mnist_inference.OUTPUT_NODE], name='y-input')
+
+    regularizer = tf.contrib.layers.l2_regularizer(REGULARIZATION_RATE)
+    # 直接使用mnist_inference.py中定义的前向传播过程
+    y = mnist_inference.inference(x, regularizer)
+
+    # 定义存储训练轮数的变量，将改变量指定为不可训练的参数
+    global_step = tf.Variable(0, trainable=False)
+    # 定义滑动平均类
+    variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
+
+    # 在所有神经网络参数的变量上使用滑动平均
+    variables_averages_op = variable_averages.apply(tf.trainable_variables())
+
+    # 定义损失函数
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(y_, 1))
+
+    # 计算在当前batch中所有样例的交叉熵平均值
+    cross_entropy_mean = tf.reduce_mean(cross_entropy)
+
+    # 总损失等于交叉熵损失和正则化损失的和
+    loss = cross_entropy_mean + tf.add_n(tf.get_collection('losses'))
+
+    # 设置指数衰减的学习率
+    learning_rate = tf.train.exponential_decay(LEARNING_RATE_BASE, global_step, mnist.train.num_examples / BATCH_SIZE, LEARNING_RATE_DECAY)
+
+    # 定义优化算法
+    train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+
+    train_op = tf.group(train_step, variables_averages_op)
+
+    # 初始化TensorFlow持久化类
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+
+        for i in range(TRAINING_STEPS):
+            xs, ys = mnist.train.next_batch(BATCH_SIZE)
+            _, loss_value, step = sess.run([train_op, loss, global_step], feed_dict={x: xs, y_: ys})
+
+            # 每1000轮保存一次模型
+            if i % 1000 == 0:
+                print('After %d training step(s), loss on training batch is %g' % (step, loss_value))
+
+                # 保存当前的模型
+                saver.save(sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME), global_step=global_step)
+
+def main(argv=None):
+    mnist = input_data.read_data_sets("data/MNIST_data", one_hot=True)
+    train(mnist)
+
+if __name__ == '__main__':
+    tf.app.run()
